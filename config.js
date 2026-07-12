f0d303a1e8acebf395c1b5c10064dd68b1028fb5
eleanorxiying98-web <eleanorxiying98@gmail.com>
2026-07-12 23:22:07 +0800

    config.js

---
+++ config.js
@@ -0,0 +1,47 @@
+const CONFIG = {
+
+    weddingDate: "19 December 2026",
+
+    hotels:{
+
+        "Movenpick":{
+
+            name:"Mövenpick Resort Khao Yai",
+
+            checkin:"19 Dec 2026 • From 3:00 PM",
+
+            checkout:"20 Dec 2026 • By 12:00 PM",
+
+            stay:"19–20 December 2026"
+
+        },
+
+        "Grand Wara":{
+
+            name:"Grand Wara Resort",
+
+            checkin:"19 Dec 2026 • From 2:00 PM",
+
+            checkout:"20 Dec 2026 • By 12:00 PM",
+
+            stay:"19–20 December 2026"
+
+        }
+
+    },
+
+    shuttles:{
+
+        "18":"18 December 2026",
+
+        "19":"19 December 2026",
+
+        "20":"20 December 2026",
+
+        "N/A":"Self-arranged",
+
+        "": "Self-arranged"
+
+    }
+
+};
